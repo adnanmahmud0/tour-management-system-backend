005a37dfdb1a93e88b246a9d8a7152eb791390cb
adnanmahmud0 <adnan99mahmud@gmail.com>
2025-07-27 05:54:06 +0600

    user setup done but some are remining

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
-
+import cors from "cors";
 import express, { Request, Response } from "express";
+// import { UserRoutes } from "./app/modules/user/user.route";
+import { router } from "./app/routes";
+
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { User } from "./user.model";
+import httpStatus from "http-status-codes";
+
+
+const createUser = async (req: Request, res: Response) => {
+    try {
+        const { name, email } = req.body;
+
+
+        const user = await User.create({
+            name,
+            email,
+        })
+
+        res.status(httpStatus.CREATED).json({
+            message: "User created successfully",
+            user
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(httpStatus.BAD_REQUEST).json({
+            message: `Error creating user!! ${error instanceof Error ? error.message : String(error)}`,
+            error
+        });
+    }
+}
+
+export const UserControllers = {
+    createUser
+}
